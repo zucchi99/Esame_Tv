7c14409e615c96376e479d4819532b171c35f9e4
Massimiliano Baldo <50354884+MaxBaldo99@users.noreply.github.com>
2020-01-17 15:02:44 +0100

    Add the methods to Registratore class

--- src/level2/Registratore.java
+++ src/level2/Registratore.java
@@ -1,25 +1,42 @@
 package level2;
+
 import level4.Trasmissione;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Registratore {
+public class Registratore implements tv.Registratore {
+    List<Trasmissione> trasmissioniFuture;
 
-    List<Trasmissione> trasmRegistate;
-    List<Trasmissione> trasmDaRegistare;
-
-    public List<Trasmissione> getTrasmRegistate() {
-        return trasmRegistate;
+    /**
+     * Aggiunge una nuova trasmissione a quelle da registrare
+     * @param tr la trasmissione da aggiugere. Required non nulla
+     */
+    @Override
+    public void aggiungiTrasmissione(tv.Trasmissione tr) throws IllegalArgumentException {
+        if (controllaPrenotazioneTrasmissione(tr)) {
+            trasmissioniFuture.add((Trasmissione) tr);
+        } else {
+            throw new IllegalArgumentException("La trasmissione non è presente nella lista delle registrazioni");
+        }
     }
 
-    public void setTrasmRegistate(List<Trasmissione> trasmRegistate) {
-        this.trasmRegistate = trasmRegistate;
+    /**
+     * Controlla se la trasmissione è presente tra quelle future da registrare
+     * @param tr è la trasmissione da controllare. required non nulla
+     * @return vero se la trasmissione appartiene a quelle future registrate
+     */
+    @Override
+    public boolean controllaPrenotazioneTrasmissione(tv.Trasmissione tr) {
+        List<Trasmissione> disponibilità = trasmissioniFuture.stream().filter(x -> x.equals(tr)).collect(Collectors.toList());
+        return disponibilità.isEmpty();
     }
 
-    public List<Trasmissione> getTrasmDaRegistare() {
-        return trasmDaRegistare;
-    }
-
-    public void setTrasmDaRegistare(List<Trasmissione> trasmDaRegistare) {
-        this.trasmDaRegistare = trasmDaRegistare;
+    @Override
+    public void eliminaTrasmissione(tv.Trasmissione tr) throws IllegalArgumentException {
+        if (controllaPrenotazioneTrasmissione(tr)) {
+            trasmissioniFuture.remove(tr);
+        } else {
+            throw new IllegalArgumentException("La trasmissione non è presente nella lista delle registrazioni");
+        }
     }
 }
